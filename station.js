6f7cbe2b24d2ef61d305512dd93e196fa14503ab
Jasnoor Kaur <jasnoorkaurjasi@gmail.com>
2026-01-28 10:20:44 +0530

    Update station.js

--- station.js
+++ station.js
@@ -1,1 +1,15 @@
+const kettle = document.querySelector(".kettle");
+const coffee = document.querySelector(".coffee");
+
+let filled = false;
+
+kettle.addEventListener("click", () => {
+  if (!filled) {
+    coffee.style.height = "70%";
+    filled = true;
+  } else {
+    coffee.style.height = "0%";
+    filled = false;
+  }
+});
 
